6737b3b005e0d3108cc6ab125c935387d31dec99
l-yusya <105113818+l-yusya@users.noreply.github.com>
2022-10-21 18:46:52 +0300

    Header done

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,5 +22,21 @@ const toggleNavbar = function () {
 
 addEventOnElem(navToggler, "click", toggleNavbar);
 
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
 
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+const header = document.querySelector("[data-header]");
+const activeElemOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeElemOnScroll)
 
